0282c5faa5ceae73ad5758d13b8312c6969fcec4
Nilton Rocha <nilton.silva@grupocard.com.br>
2022-04-14 01:31:00 -0300

    primeiro commit com estrura MVC completa 13/04/2022 01:30

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,8 +2,38 @@
 
 const indexController = {
     index: async (req, res) => {
+
+        const faqs = [           {         
+                id: 1
+            },
+            {
+                id: 2
+            },
+            {         
+                id: 3
+            },
+            {
+                id: 4
+            },
+            {         
+                id: 5
+            },
+            {
+                id: 6
+            },
+            {         
+                id: 7
+            },
+            {
+                id: 9
+            }
+        ]
+
+        
+
         res.render('index', {
-            title: "A transportadora do futuro"
+            title: "A transportadora do futuro",
+            faqs
         })
     }
 }
